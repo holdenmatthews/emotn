c3604ba208719b697ac6145e51142111cabbc61c
Holden Matthews <holdenmatthews1@gmail.com>
2023-03-28 15:34:34 -0600

    LogSearch created. startDate and endDate state set up.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,20 @@ import { useState, useContext, useEffect } from 'react'
 import AuthContext from '../store/authContext'
 import axios from 'axios'
 import LogDisplay from './LogDisplay'
+import LogSearch from './LogSearch'
 
 const Home = () => {
   const [userLogs, setUserLogs] = useState([])
   const { token, userId } = useContext(AuthContext)
+  const [startDate, setStartDate] = useState("")
+  const [endDate, setEndDate] = useState("")
 
   const getUserLogs = () => {
-    axios.get(`http://localhost:4444/api/logs/${userId}`)
+    axios.get(`http://localhost:4444/api/logs/${userId}`, {
+      headers: {
+        authorization: token
+      }
+    })
     .then((res) => {
       // console.log(res.data)
       setUserLogs(res.data)
@@ -34,6 +41,7 @@ const Home = () => {
 
   return (
     <div>
+      <LogSearch startDate={startDate} setStartDate={setStartDate} endDate={endDate} setEndDate={setEndDate} />
       <LogDisplay userLogs={userLogs} key={1} deleteLog={deleteLog} />
     </div>
   )
---
+++ src/components/LogSearch.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const LogSearch = (props) => {
+    const { startDate, setStartDate, endDate, setEndDate } = props
+
+    const setMin = (e) => {
+        setStartDate(e.target.value)
+        // console.log(startDate)
+    }
+
+    const setMax = (e) => {
+        setEndDate(e.target.value)
+        // console.log(endDate)
+    }
+
+  return (
+    <div>
+      <h3>Search for logs within a range of dates(included):</h3>
+      <label htmlFor="datemin">Range Start</label>
+      <input type="date" name="datemin" value={startDate} onChange={(e) => setMin(e)} />
+      <label htmlFor="datemax">Range End</label>
+      <input type="date" name="datemax" value={endDate} onChange={(e) => setMax(e)} />
+    </div>
+  );
+};
+
+export default LogSearch;
